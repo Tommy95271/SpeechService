b7213293899efe5a12adc6701ef1325156a779ae
Tommy_Hsieh <Tommy_Hsieh@everrich.com.tw>
2024-02-27 15:41:32 +0800

    feature : 加入麥克風收音方法

--- SpeechServices/Services/SpeechService.cs
+++ SpeechServices/Services/SpeechService.cs
@@ -14,9 +14,10 @@ namespace SpeechServices.Services
         {
             var speechConfig = SpeechConfig.FromSubscription("key", "region");
             speechConfig.SpeechRecognitionLanguage = "zh-TW";
-            speechConfig.SetProperty(PropertyId.Speech_SegmentationSilenceTimeoutMs, "5000");
+            speechConfig.SetProperty(PropertyId.Speech_SegmentationSilenceTimeoutMs, "1000");
             speechConfig.SetProperty(PropertyId.Speech_LogFilename, "./mylog2");
-            await FromFile(speechConfig);
+            //await FromFile(speechConfig);
+            await FromMicrophone(speechConfig);
         }
 
         internal async Task FromFile(SpeechConfig speechConfig)
@@ -30,6 +31,60 @@ namespace SpeechServices.Services
             //{
             //    Console.WriteLine($"RECOGNIZING: Text={e.Result.Text}");
             //};
+
+            speechRecognizer.Recognized += (s, e) =>
+            {
+                if (e.Result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    Console.WriteLine($"RECOGNIZED: Text={e.Result.Text}");
+                    File.AppendAllLinesAsync("./output.txt", new List<string> { e.Result.Text });
+                }
+                else if (e.Result.Reason == ResultReason.NoMatch)
+                {
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                }
+            };
+
+            speechRecognizer.Canceled += (s, e) =>
+            {
+                Console.WriteLine($"CANCELED: Reason={e.Reason}");
+
+                if (e.Reason == CancellationReason.Error)
+                {
+                    Console.WriteLine($"CANCELED: ErrorCode={e.ErrorCode}");
+                    Console.WriteLine($"CANCELED: ErrorDetails={e.ErrorDetails}");
+                    Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
+                }
+
+                stopRecognition.TrySetResult(0);
+            };
+
+            speechRecognizer.SessionStopped += (s, e) =>
+            {
+                Console.WriteLine("\n    Session stopped event.");
+                stopRecognition.TrySetResult(0);
+            };
+
+            await speechRecognizer.StartContinuousRecognitionAsync();
+
+            // Waits for completion. Use Task.WaitAny to keep the task rooted.
+            Task.WaitAny(new[] { stopRecognition.Task });
+
+            // Make the following call at some point to stop recognition:
+            // await speechRecognizer.StopContinuousRecognitionAsync();
+
+
+            //var result = await speechRecognizer.RecognizeOnceAsync();
+            //Console.WriteLine($"RECOGNIZED: Text={result}");
+        }
+
+        internal async Task FromMicrophone(SpeechConfig speechConfig)
+        {
+            using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+            Console.WriteLine("Speak into your microphone.");
+            var stopRecognition = new TaskCompletionSource<int>();
 
             speechRecognizer.Recognized += (s, e) =>
             {
