d510ad84560d5398b690f66eb25ce22f2b838d8b
Tommy_Hsieh <Tommy_Hsieh@everrich.com.tw>
2024-02-29 12:20:10 +0800

    feature : 拆分語音服務類別庫翻譯及播放語音方法

---
+++ SpeechLibrary/Services/SpeechService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech.Translation;
+
+namespace SpeechLibrary.Services
+{
+    public class SpeechService
+    {
+        public async Task<string?> TranslateFromMicrophone(SpeechTranslationConfig speechTranslationConfig)
+        {
+            using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            using var translationRecognizer = new TranslationRecognizer(speechTranslationConfig, audioConfig);
+
+            var result = await translationRecognizer.RecognizeOnceAsync();
+            return result.Translations.Values.FirstOrDefault();
+        }
+
+        public async Task PlayTextAsAudio(SpeechTranslationConfig speechTranslationConfig, string? text)
+        {
+            using var speechSynthesizer = new SpeechSynthesizer(speechTranslationConfig);
+            using var result = await speechSynthesizer.SpeakTextAsync(text);
+            if (result.Reason == ResultReason.SynthesizingAudioCompleted)
+            {
+                // Audio playback completed
+            }
+            else if (result.Reason == ResultReason.Canceled)
+            {
+                var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
+                // Handle cancellation
+            }
+        }
+    }
+}
