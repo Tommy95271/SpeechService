ac034e060bd463a166acc81979fcaddfcf2646b3
Tommy_Hsieh <Tommy_Hsieh@everrich.com.tw>
2024-03-01 18:01:10 +0800

    feature : Speech Blazor WebAssembly 專案新增畫面及元件

---
+++ SpeechWASM/Components/Card.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SpeechWASM.Components
+{
+    public partial class Card
+    {
+        [Parameter]
+        public int Order { get; set; }
+        [Parameter]
+        public string? Text { get; set; }
+        [Parameter]
+        public string? Translation { get; set; }
+    }
+}
---
+++ SpeechWASM/Pages/Speech/SpeechPage.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using SpeechLibrary.Models;
+using System.Net.Http.Json;
+
+namespace SpeechWASM.Pages.Speech
+{
+    public partial class SpeechPage
+    {
+        [Inject]
+        public HttpClient httpClient { get; set; }
+        public int Order { get; set; }
+
+        public List<SpeechModel> SpeechModels { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            SpeechModels = new List<SpeechModel>();
+            Order = 1;
+        }
+
+        private async Task Translate()
+        {
+            var result = await httpClient.GetFromJsonAsync<SpeechModel>("Speech/TranslateFromMicrophoneAndPlay");
+            SpeechModels.Add(new SpeechModel
+            {
+                Id = result.Id,
+                Order = Order++,
+                Text = result.Text,
+                Translation = result.Translation,
+            });
+        }
+    }
+}
